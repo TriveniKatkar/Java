45cc827afafe85128aceb82197eb0d235ba98727
LENOVO <LENOVO@LENOVO-PC-2>
2024-02-26 15:46:33 +0530

    COMMIT

---
+++ Array.java/src/com/feb24/Array1.java
@@ -0,0 +1,15 @@
+package com.feb24;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50};
+		System.out.println(arr[4]);
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println();
+		}
+		
+	}
+
+}
